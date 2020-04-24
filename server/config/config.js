e4c6bbe67d4ebb78347a25ff93191680db59074f
NandoAM <fagudom@gmail.com>
2020-04-24 07:10:50 +0200

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 //====================================
+// Vencimiento token (30 DÍAS =>  60 segundos * 60 minutos * 24 horas * 30 días)
+//====================================
+
+process.env.CADUCIDA_TOKEN = 60 * 60 * 24 * 30; 
+
+//====================================
+// SEED de autenticación (process.env.SEED => Si se sube a heroku hay que crear una variable de entorno con la de producción)
+//====================================
+process.env.SEED = process.env.SEED ||  'este-es-el-seed-desarrollo';
+
+//====================================
 // Base de datos
 //====================================
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,9 +4,11 @@ const _ = require('underscore');
 
 const Usuario = require('../models/usuarios');
 
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario',verificaToken ,(req, res)  => {
 
     let desde = req.query.desde || 0;
 
@@ -44,7 +46,7 @@ app.get('/usuario', function (req, res) {
 
 });
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken,verificaAdmin_Role] , (req, res) => {
 
     let body = req.body;
 
@@ -74,7 +76,7 @@ app.post('/usuario', function (req, res) {
 
 });
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id',[verificaToken,verificaAdmin_Role] , (req, res) =>{
 
     let id = req.params.id;
 
@@ -113,7 +115,7 @@ app.put('/usuario/:id', function (req, res) {
 
 });
 
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id',[verificaToken,verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
 
